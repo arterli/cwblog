b67fdde9f4ea6aec2525768e6694ff4a6ee8dfae
Arterli <arterli@qq.com>
2015-04-24 03:02:42 +0800

    发布文章

--- App/Lib/Controller/Admin/ArticleController.js
+++ App/Lib/Controller/Admin/ArticleController.js
@@ -10,116 +10,39 @@ module.exports = Controller("Admin/BaseController" , function(){
 
       },
         dataAction:function(){
-                var data={
-                    "total": 800,
-                    "rows":[
-                    {
-                        "id": 0,
-                        "name": "Item 0",
-                        "price": "$0"
-                    },
-                    {
-                        "id": 1,
-                        "name": "Item 1",
-                        "price": "$1"
-                    },
-                    {
-                        "id": 2,
-                        "name": "Item 2",
-                        "price": "$2"
-                    },
-                    {
-                        "id": 3,
-                        "name": "Item 3",
-                        "price": "$3"
-                    },
-                    {
-                        "id": 4,
-                        "name": "Item 4",
-                        "price": "$4"
-                    },
-                    {
-                        "id": 5,
-                        "name": "Item 5",
-                        "price": "$5"
-                    },
-                    {
-                        "id": 6,
-                        "name": "Item 6",
-                        "price": "$6"
-                    },
-                    {
-                        "id": 7,
-                        "name": "Item 7",
-                        "price": "$7"
-                    },
-                    {
-                        "id": 8,
-                        "name": "Item 8",
-                        "price": "$8"
-                    },
-                    {
-                        "id": 9,
-                        "name": "Item 9",
-                        "price": "$9"
-                    },
-                    {
-                        "id": 10,
-                        "name": "Item 10",
-                        "price": "$10"
-                    },
-                    {
-                        "id": 11,
-                        "name": "Item 11",
-                        "price": "$11"
-                    },
-                    {
-                        "id": 12,
-                        "name": "Item 12",
-                        "price": "$12"
-                    },
-                    {
-                        "id": 13,
-                        "name": "Item 13",
-                        "price": "$13"
-                    },
-                    {
-                        "id": 14,
-                        "name": "Item 14",
-                        "price": "$14"
-                    },
-                    {
-                        "id": 15,
-                        "name": "Item 15",
-                        "price": "$15"
-                    },
-                    {
-                        "id": 16,
-                        "name": "Item 16",
-                        "price": "$16"
-                    },
-                    {
-                        "id": 17,
-                        "name": "Item 17",
-                        "price": "$17"
-                    },
-                    {
-                        "id": 18,
-                        "name": "Item 18",
-                        "price": "$18"
-                    },
-                    {
-                        "id": 19,
-                        "name": "Item 19",
-                        "price": "$19"
-                    },
-                    {
-                        "id": 20,
-                        "name": "Item 20",
-                        "price": "$20"
-                    }
-                ]}
-                this.json(data)
+            var self=this;
+            var page=(this.get("offset")/10)+1;
+            var limit=this.get("limit");
+            D('Article').page(page, limit).countSelect().then(function(da){
+                //data的数据格式为
+                var datas={};
+                datas.total=da.count;
+
+                var rows=[];
+                da.data.forEach(function(item){
+                     var date=new Date(item.date);
+                    var y=date.getFullYear();
+                    var M=date.getMonth()+1;
+                    M=M<10?"0"+M:M;
+                    var d=date.getDate();
+                    d=d<10?"0"+d:d;
+                    var h=date.getHours();
+                    h=h<10?"0"+h:h;
+                    var m=date.getMinutes();
+                    m=m<10?"0"+m:m;
+                    var time= y+"-"+M+"-"+d+" "+h+":"+m;
+                    var author=D('user').where({uid:item.author}).field('username').find().then(function(data){
+                      data.username;
+                    });
+                    rows.push({date:time,gid:item.gid,title:item.title,type:item.type});
+                    console.log(author)
+                })
+
+                datas.rows=rows;
+
+               // console.log(datas);
+                self.json(datas)
+            });
 
         },
         addarticleAction:function(){
